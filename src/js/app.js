de9a18b8d8358b32649667a9ab4c2c6f0b6d4ca9
rabbitbooks <catdelion@mail.ru>
2019-05-15 10:49:00 +0300

    'workspace deployed/slider is configured'

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,31 @@
-const x = 3;
-console.log(x);
+const initPageSlider = () => {
+	const $slider = $('.js-page-slider');
+	$slider.slick({
+		arrows: false,
+		vertical: true
+	})
+
+	$slider.mousewheel((e) => {
+		e.preventDefault()
+		if (e.deltaY < 0) {
+			$slider.slick('slickNext')
+		} else {
+			$slider.slick('slickPrev')
+		}
+	})
+
+	$slider.swipe({
+		swipe: (e, direction, distance, duration, fingerCount, fingerData) => {
+			if (direction == 'up') {
+				$slider.slick('slickNext')
+			} else {
+				$slider.slick('slickPrev')
+			}
+		},
+		treshold: 0
+	})
+}
+
+$(document).ready(() => {
+	initPageSlider()
+})
